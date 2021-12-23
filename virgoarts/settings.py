bb2e6b743b9819cf1c5797aabfd2c08c4ff17941
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2021-12-23 01:35:18 +0100

    Update settings.py

--- virgoarts/settings.py
+++ virgoarts/settings.py
@@ -52,7 +52,7 @@ SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
