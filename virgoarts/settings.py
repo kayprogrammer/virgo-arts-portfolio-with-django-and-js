d29d490c71fba2ada1b3ffb60ccafc3610be71c0
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2021-12-23 02:06:00 +0100

    Update settings.py

--- virgoarts/settings.py
+++ virgoarts/settings.py
@@ -145,7 +145,7 @@ SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
